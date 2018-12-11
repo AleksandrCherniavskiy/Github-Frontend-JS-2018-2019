a17f8b1b14343d7a0e477c274cd1a4c7961d348a
AleksandrCherniavskiy <43580496+AleksandrCherniavskiy@users.noreply.github.com>
2018-12-12 01:26:26 +0200

    Added new ideas

--- HW7/game.js
+++ HW7/game.js
@@ -25,3 +25,152 @@ while (true) {
       south();
     }
 };
+
+/*level 5*/
+while (true) {
+    if ( isFree('east') ) {
+        east();
+    } else if ( isFree('south') ) {
+        south();
+    } else if ( isFree('west') ) {
+        west();
+    } else {
+        north();
+    }
+};
+
+while (isFree('east') !== "next") {
+    east();
+}
+
+let i;
+while (i !== 'next') {
+    if ( isFree('east') ) {
+       i = east();
+    } else if ( isFree('south') ) {
+        i = south();
+    } else if ( isFree('west') ) {
+        i = west();
+    } else {
+        i = north();
+    }
+};
+
+for (let i; i !== 'next';) {
+    i = north();
+}
+
+for (let i = true; i === true;) {
+    i = north();
+}
+
+for (let i; i !== 'next';) {
+    if ( isFree('north') ) {
+        alert ('north');
+    }
+    else if ( isFree('east') ) {
+        i = east();
+    }
+}
+
+/*next*/
+for (let i; i !== 'next';) {
+    if ( isFree('east') ) {
+        i = east();
+    } else if ( isFree('south') ) {
+        i = south();
+    } else if ( isFree('west') ) {
+        i = west();
+    } else {
+        i = north();
+    }
+}
+
+/*next2*/
+for (let i; i !== 'next';) {
+    while ( isFree('east') ) {
+        i = east();
+    };
+    while ( isFree('south') ) {
+        i = south();
+    };
+    while ( isFree('west') ) {
+        i = west();
+    };
+    while ( isFree('north') ) {
+        i = north();
+    };
+}
+
+/*21*/
+let east = isFree('east');
+for (let i = true; i !== 'next';) {
+    if ( isFree('east') ) {
+        while ( isFree('east') ) {
+            i = east();
+        }
+        east = false;
+    }
+    else if ( isFree('west') ) {
+        while ( isFree('west') ) {
+            i = west();
+        }
+    }
+    else if ( isFree('south') ) {
+        while ( isFree('south') ) {
+            i = south();
+        }
+    }
+    else if ( isFree('north') ){
+        while ( isFree('north') ) {
+            i = north();
+        }
+
+    }
+}
+
+/*22*/
+let east = isFree('east');
+let west = isFree('west');
+let south = isFree('south');
+let north = isFree('north');
+
+for (; i !== 'next';) {
+    if ( east ) {
+        while ( east ) {
+            east();
+        }
+        east = false;
+    }
+    else if ( isFree('west') ) {
+        while ( isFree('west') ) {
+            i = west();
+        }
+    }
+    else if ( isFree('south') ) {
+        while ( isFree('south') ) {
+            i = south();
+        }
+    }
+    else if ( isFree('north') ){
+        while ( isFree('north') ) {
+            i = north();
+        }
+
+    }
+}
+/*
+for (let i = true; i !== 'next';) {
+        while ( i === true ) {
+            i = east();
+        }
+        while ( i === true  ) {
+            i = west();
+        }
+        while ( i === true  ) {
+            i = south();
+        }
+        while ( i === true  ) {
+            i = north();
+        }
+    }*/
